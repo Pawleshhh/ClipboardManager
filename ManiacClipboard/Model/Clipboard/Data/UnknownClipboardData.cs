d82ce8a3411e9ffd67b460a130a0f23bbdfe0181
Pawleshhh <mateusz46668@gmail.com>
2020-02-15 15:58:40 +0100

    Added the UnknownClipboardData class (without its unit tests).

---
+++ ManiacClipboard/Model/Clipboard/Data/UnknownClipboardData.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace ManiacClipboard.Model
+{
+
+    /// <summary>
+    /// Represents data that can be stored on the clipboard and its type is unknown.
+    /// </summary>
+    public sealed class UnknownClipboardData : ClipboardData<object>
+    {
+
+        #region Constructors
+
+        public UnknownClipboardData(object data, string[] formats) : base(data, ClipboardDataType.Unknown)
+        {
+            Initialize(formats);
+        }
+
+        public UnknownClipboardData(object data, string[] formats, DateTime copyTime) : base(data, ClipboardDataType.Unknown, copyTime)
+        {
+            Initialize(formats);
+        }
+
+        public UnknownClipboardData(object data, string[] formats, ClipboardSource source) : base(data, ClipboardDataType.Unknown, source)
+        {
+            Initialize(formats);
+        }
+
+        public UnknownClipboardData(object data, string[] formats, DateTime copyTime, ClipboardSource source) : base(data, ClipboardDataType.Unknown, copyTime, source)
+        {
+            Initialize(formats);
+        }
+
+        private void Initialize(string[] formats)
+        {
+            _formats = formats;
+            _isDisposable = Data is IDisposable;
+
+            if (!_isDisposable)
+                GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        #region Private fields
+
+        private string[] _formats;
+
+        private string _name;
+
+        private bool _isDisposed;
+
+        private bool _isDisposable;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets array of formats of the unknown stored data.
+        /// </summary>
+        public string[] GetFormats()
+        {
+            string[] array = new string[_formats.Length];
+
+            Array.Copy(_formats, 0, array, 0, _formats.Length);
+
+            return array;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            return Data.Equals(obj);
+        }
+
+        public override int GetHashCode() => Data.GetHashCode() * 13 * 17;
+
+        public override string ToString()
+        {
+            if (_name == null)
+                _name = Data.GetType().Name;
+
+            return _name;
+        }
+
+        public override void Dispose()
+        {
+            if(_isDisposable && !_isDisposed)
+            {
+                ((IDisposable)Data).Dispose();
+                _isDisposed = true;
+            }
+        }
+
+        ~UnknownClipboardData()
+        {
+            Dispose();
+        }
+
+        #endregion
+
+    }
+}
