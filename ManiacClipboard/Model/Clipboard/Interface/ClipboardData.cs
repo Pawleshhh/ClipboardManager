9006d90353a6693cd0070fba7780233417807f6a
Pawleshhh <mateusz46668@gmail.com>
2020-02-15 12:19:45 +0100

    Added the ClipboardData class.

---
+++ ManiacClipboard/Model/Clipboard/Interface/ClipboardData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ManiacClipboard.Model
+{
+    /// <summary>
+    /// Represents data that can be stored on the clipboard.
+    /// </summary>
+    public abstract class ClipboardData : IDisposable
+    {
+        #region Constructors
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Disposes stored data.
+        /// </summary>
+        public virtual void Dispose() { }
+
+        #endregion
+
+    }
+}
