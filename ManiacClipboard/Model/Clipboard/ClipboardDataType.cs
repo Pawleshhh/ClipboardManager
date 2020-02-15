efc4a0f65b9e92f2cd8e4d1e6024cdece737ab04
Pawleshhh <mateusz46668@gmail.com>
2020-02-15 14:20:53 +0100

    Changed location of the ClipboardDatType enum.

---
+++ ManiacClipboard/Model/Clipboard/ClipboardDataType.cs
@@ -0,0 +1,34 @@
+﻿namespace ManiacClipboard.Model
+{
+
+    /// <summary>
+    /// Enum values that indicate certain clipboard data type.
+    /// </summary>
+    public enum ClipboardDataType
+    {
+        /// <summary>
+        /// Unknown data.
+        /// </summary>
+        Unknown = 0,
+        /// <summary>
+        /// Text data.
+        /// </summary>
+        Text,
+        /// <summary>
+        /// Text which contains path to specified file.
+        /// </summary>
+        FilePath,
+        /// <summary>
+        /// Collection of paths to specified files.
+        /// </summary>
+        FileList,
+        /// <summary>
+        /// Image data.
+        /// </summary>
+        Image,
+        /// <summary>
+        /// Audio data.
+        /// </summary>
+        Audio
+    }
+}
