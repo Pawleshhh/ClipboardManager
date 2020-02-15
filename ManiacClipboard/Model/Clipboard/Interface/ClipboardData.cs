24c1a34beb0b51659f4a4e81bfa9499f722b430c
Pawleshhh <mateusz46668@gmail.com>
2020-02-15 12:28:51 +0100

    Added the ClipboardDataType enum.

--- ManiacClipboard/Model/Clipboard/Interface/ClipboardData.cs
+++ ManiacClipboard/Model/Clipboard/Interface/ClipboardData.cs
@@ -9,6 +9,10 @@ namespace ManiacClipboard.Model
     {
         #region Constructors
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ClipboardData"/> class.
+        /// </summary>
+        /// <param name="data">Data to be stored.</param>
         protected ClipboardData(object data)
         {
 
@@ -29,5 +33,36 @@ namespace ManiacClipboard.Model
 
         #endregion
 
+    }
+
+    /// <summary>
+    /// Enum values that indicate certain clipboard data type.
+    /// </summary>
+    public enum ClipboardDataType
+    {
+        /// <summary>
+        /// Unknown data.
+        /// </summary>
+        Unknown = 0,
+        /// <summary>
+        /// Text data.
+        /// </summary>
+        Text,
+        /// <summary>
+        /// Text which contains path to specified file.
+        /// </summary>
+        FilePath,
+        /// <summary>
+        /// Collection of paths to specified files.
+        /// </summary>
+        FileList,
+        /// <summary>
+        /// Image data.
+        /// </summary>
+        Image,
+        /// <summary>
+        /// Audio data.
+        /// </summary>
+        Audio
     }
 }
