c6b5da1a8a3a19bbacadca410ae65c35bc5414d5
Pawleshhh <mateusz46668@gmail.com>
2020-02-15 15:30:33 +0100

    Added the TextClipboardData class and its unit tests.

---
+++ ManiacClipboard/Model/Clipboard/Data/TextClipboardData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ManiacClipboard.Model
+{
+    /// <summary>
+    /// Text type of clipboard data.
+    /// </summary>
+    public class TextClipboardData : ClipboardData<string>, IEquatable<TextClipboardData>
+    {
+        public TextClipboardData(string data) : base(data, ClipboardDataType.Text)
+        {
+        }
+
+        public TextClipboardData(string data, DateTime copyTime) : base(data, ClipboardDataType.Text, copyTime)
+        {
+        }
+
+        public TextClipboardData(string data, ClipboardSource source) : base(data, ClipboardDataType.Text, source)
+        {
+        }
+
+        public TextClipboardData(string data, DateTime copyTime, ClipboardSource source) : base(data, ClipboardDataType.Text, copyTime, source)
+        {
+        }
+
+        public bool Equals(TextClipboardData other)
+            => other != null && Data.Equals(other.Data);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj is TextClipboardData text)
+                return Equals(text);
+
+            return false;
+        }
+
+        public override int GetHashCode() => Data.GetHashCode() * 17;
+    }
+}
---
+++ ManiacClipboardTests/Model/Clipboard/Data/TextClipboardDataTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ManiacClipboard.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManiacClipboard.Model.Tests
+{
+    [TestClass()]
+    public class TextClipboardDataTests
+    {
+
+        [TestMethod]
+        public void IEquatableEquals_CheckingEqualityWhenParameterIsNull_ReturnsFalse()
+        {
+            var clipboardData = new TextClipboardData("text");
+
+            bool result = clipboardData.Equals(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [DataTestMethod]
+        [DataRow("data", "data", true)]
+        [DataRow("data1", "data2", false)]
+        public void IEquatableEquals_CheckingEquality_ReturnsExpectedValue(string data1, string data2, bool expected)
+        {
+            var clipboardData1 = new TextClipboardData(data1);
+            var clipboardData2 = new TextClipboardData(data2);
+
+            bool result = clipboardData1.Equals(clipboardData2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Equals_CheckingEqualityWhenParameterIsNull_ReturnsFalse()
+        {
+            object clipboardData = new TextClipboardData("text");
+
+            bool result = clipboardData.Equals(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [DataTestMethod]
+        [DataRow("data", "data", true)]
+        [DataRow("data1", "data2", false)]
+        public void Equals_CheckingEquality_ReturnsExpectedValue(string data1, string data2, bool expected)
+        {
+            object clipboardData1 = new TextClipboardData(data1);
+            object clipboardData2 = new TextClipboardData(data2);
+
+            bool result = clipboardData1.Equals(clipboardData2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Equals_CheckingEqualitWhenObjectsAreSame_ReturnsTrue()
+        {
+            object clipboardData1 = new TextClipboardData("data");
+            object clipboardData2 = clipboardData1;
+
+            bool result = clipboardData1.Equals(clipboardData2);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Equals_CheckingEqualityWhenObjectsAreNotTheSameType_ReturnsFalse()
+        {
+            object clipboardData1 = new TextClipboardData("data");
+            object clipboardData2 = new object();
+
+            bool result = clipboardData1.Equals(clipboardData2);
+
+            Assert.IsFalse(result);
+        }
+
+    }
+}
