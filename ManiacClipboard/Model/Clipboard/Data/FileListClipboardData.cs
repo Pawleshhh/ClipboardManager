10375fd734691830c0b3c97bbe8a846f76a9d27d
Pawleshhh <mateusz46668@gmail.com>
2020-02-15 17:20:30 +0100

    Added the FileListClipboardData class and its unit tests.

---
+++ ManiacClipboard/Model/Clipboard/Data/FileListClipboardData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManiacClipboard.Model
+{
+    /// <summary>
+    /// Contains data with multiple paths to files and directories that can be stored on the clipboard.
+    /// </summary>
+    public class FileListClipboardData : CollectionClipboardData<KeyValuePair<string, bool>>
+    {
+
+        #region Constructors
+
+        protected FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data, ClipboardDataType type) :
+            base(data, type)
+        {
+        }
+
+        protected FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data, ClipboardDataType type, DateTime copyTime) :
+            base(data, type, copyTime)
+        {
+        }
+
+        protected FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data, ClipboardDataType type, ClipboardSource source) :
+            base(data, type, source)
+        {
+        }
+
+        protected FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data, ClipboardDataType type, DateTime copyTime, ClipboardSource source) :
+            base(data, type, copyTime, source)
+        {
+        }
+
+        public FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data) :
+            this(data, ClipboardDataType.FileList)
+        {
+        }
+
+        public FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data, DateTime copyTime) :
+            this(data, ClipboardDataType.FileList, copyTime)
+        {
+        }
+
+        public FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data, ClipboardSource source) :
+            this(data, ClipboardDataType.FileList, source)
+        {
+        }
+
+        public FileListClipboardData(IReadOnlyCollection<KeyValuePair<string, bool>> data, DateTime copyTime, ClipboardSource source) :
+            this(data, ClipboardDataType.FileList, copyTime, source)
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override ClipboardData[] Split()
+            => Data.Select(n => new PathClipboardData(n.Key, n.Value, CopyTime, Source) { KeepThat = this.KeepThat}).ToArray();
+
+        public override string ToString()
+        {
+            return "Collection of paths";
+        }
+
+        #endregion
+    }
+}
---
+++ ManiacClipboardTests/Model/Clipboard/Data/FileListClipboardDataTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ManiacClipboard.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManiacClipboard.Model.Tests
+{
+    [TestClass()]
+    public class FileListClipboardDataTests
+    {
+        [TestMethod]
+        public void Split_SplittingCollectionData_GetsExpectedArrayOfData()
+        {
+            ClipboardSource source = new ClipboardSource("appName");
+            DateTime dateTime = new DateTime(2020, 1, 1);
+            var clipboardData = new FileListClipboardData(new KeyValuePair<string, bool>[]
+            {
+                new KeyValuePair<string, bool>("path1", true)
+            }, dateTime, source)
+            {
+                KeepThat = true
+            };
+
+            var result = clipboardData.Split();
+
+            Assert.IsTrue(((PathClipboardData)result[0]).IsDirectory);
+            Assert.AreSame(source, result[0].Source);
+            Assert.AreEqual(dateTime, result[0].CopyTime);
+            Assert.AreEqual("path1", result[0].Data);
+            Assert.IsTrue(result[0].KeepThat);
+        }
+    }
+}
