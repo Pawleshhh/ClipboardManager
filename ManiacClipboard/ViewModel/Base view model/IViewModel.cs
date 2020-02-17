53250a9ddfefa0cfca334a88ae4c1faa98d5c843
Pawleshhh <mateusz46668@gmail.com>
2020-02-17 15:01:23 +0100

    Added base view model classes.

---
+++ ManiacClipboard/ViewModel/Base view model/IViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ManiacClipboard.ViewModel
+{
+    /// <summary>
+    /// Defines interface for every view model.
+    /// </summary>
+    public interface IViewModel
+    {
+        /// <summary>
+        /// Gets owner of this view model.
+        /// </summary>
+        IViewModel Owner { get; }
+    }
+}
