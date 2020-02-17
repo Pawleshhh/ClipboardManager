3b7daaeb32a901ad3ab48bb2ef68c30950e8cb74
Pawleshhh <mateusz46668@gmail.com>
2020-02-17 12:35:04 +0100

    Added the ImageClipboardData class with its unit tests.

---
+++ ManiacClipboard/Model/Clipboard/Data/ImageClipboardData.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace ManiacClipboard.Model
+{
+    /// <summary>
+    /// Stores image data that can be stored on the clipboard.
+    /// </summary>
+    public class ImageClipboardData : ClipboardData<MemoryStream>, IEquatable<ImageClipboardData>
+    {
+
+        #region Constructors
+
+        protected ImageClipboardData(MemoryStream data, ClipboardDataType type) : base(data, type)
+        {
+        }
+
+        protected ImageClipboardData(MemoryStream data, ClipboardDataType type, DateTime copyTime) : base(data, type, copyTime)
+        {
+        }
+
+        protected ImageClipboardData(MemoryStream data, ClipboardDataType type, ClipboardSource source) : base(data, type, source)
+        {
+        }
+
+        protected ImageClipboardData(MemoryStream data, ClipboardDataType type, DateTime copyTime, ClipboardSource source) : base(data, type, copyTime, source)
+        {
+        }
+
+        public ImageClipboardData(MemoryStream data) : base(data, ClipboardDataType.Image)
+        {
+        }
+
+        public ImageClipboardData(MemoryStream data, DateTime copyTime) : base(data, ClipboardDataType.Image, copyTime)
+        {
+        }
+
+        public ImageClipboardData(MemoryStream data, ClipboardSource source) : base(data, ClipboardDataType.Image, source)
+        {
+        }
+
+        public ImageClipboardData(MemoryStream data, DateTime copyTime, ClipboardSource source) : base(data, ClipboardDataType.Image, copyTime, source)
+        {
+        }
+
+        #endregion
+
+        #region Private fields
+
+        private bool _isDisposed;
+
+        private int _hashCode;
+
+        private bool _hashCodeCalculated;
+
+        #endregion
+
+        #region Public methods
+
+        public bool Equals([AllowNull] ImageClipboardData other)
+        {
+            return other != null && GetHashCode() == other.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj is ImageClipboardData img)
+                return Equals(img);
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if(!_hashCodeCalculated)
+            {
+                string bStr = Convert.ToBase64String(Data.ToArray());
+                _hashCode = bStr.GetHashCode() * 27;
+                _hashCodeCalculated = true;
+            }
+
+            return _hashCode;
+        }
+
+        public override void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                Data.Dispose();
+                GC.SuppressFinalize(this);
+                _isDisposed = true;
+            }
+        }
+
+        ~ImageClipboardData()
+        {
+            Dispose();
+        }
+
+        #endregion
+
+    }
+}
---
+++ ManiacClipboardTests/Model/Clipboard/Data/ImageClipboardDataTests.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ManiacClipboard.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ManiacClipboard.Model.Tests
+{
+    [TestClass()]
+    public class ImageClipboardDataTests
+    {
+
+        [TestMethod]
+        public void Eqauls_CheckingEqualityWhenParameterIsNull_ReturnsFalse()
+        {
+            using (MemoryStream ms1 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            {
+                var clipboardData1 = new ImageClipboardData(ms1);
+
+                bool result = clipboardData1.Equals(null);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void Eqauls_CheckingEqualityWhenImagesAreEqual_ReturnsTrue()
+        {
+            using (MemoryStream ms1 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            using (MemoryStream ms2 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            {
+                var clipboardData1 = new ImageClipboardData(ms1);
+                var clipboardData2 = new ImageClipboardData(ms2);
+
+                bool result = clipboardData1.Equals(clipboardData2);
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public void Eqauls_CheckingEqualityWhenImagesAreNotEqual_ReturnsFalse()
+        {
+            using (MemoryStream ms1 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            using (MemoryStream ms2 = new MemoryStream(new byte[] { 3, 1, 1 }))
+            {
+                var clipboardData1 = new ImageClipboardData(ms1);
+                var clipboardData2 = new ImageClipboardData(ms2);
+
+                bool result = clipboardData1.Equals(clipboardData2);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void OverriddenEquals_CheckingEqualityWhenImagesAreEqual_ReturnsTrue()
+        {
+            using (MemoryStream ms1 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            using (MemoryStream ms2 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            {
+                var clipboardData1 = new ImageClipboardData(ms1);
+                object clipboardData2 = new ImageClipboardData(ms2);
+
+                bool result = clipboardData1.Equals(clipboardData2);
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public void OverriddenEquals_CheckingEqualityWhenImagesAreNotEqual_ReturnsFalse()
+        {
+            using (MemoryStream ms1 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            using (MemoryStream ms2 = new MemoryStream(new byte[] { 3, 1, 1 }))
+            {
+                var clipboardData1 = new ImageClipboardData(ms1);
+                object clipboardData2 = new ImageClipboardData(ms2);
+
+                bool result = clipboardData1.Equals(clipboardData2);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void OverriddenEquals_CheckingEqualityWhenParameterIsNull_ReturnsFalse()
+        {
+            using(MemoryStream ms = new MemoryStream(new byte[] { 1, 1, 1 }))
+            {
+                var clipboardData = new ImageClipboardData(ms);
+                object obj = null;
+
+                bool result = clipboardData.Equals(obj);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void OverriddenEquals_CheckingEqualityWhenObjectsAreTheSame_ReturnsTrue()
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                var clipboardData1 = new ImageClipboardData(ms);
+                object clipboardData2 = clipboardData1;
+
+                bool result = clipboardData1.Equals(clipboardData2);
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public void OverriddenEquals_CheckingEqualityWhenObjectsAreNotTheSameType_ReturnsFalse()
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                var clipboardData = new ImageClipboardData(ms);
+                object obj = new object();
+
+                bool result = clipboardData.Equals(obj);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void OverriddenEquals_CheckingEqualityhWhenObjectsAreEqual_ReturnsTrue()
+        {
+            using (MemoryStream ms1 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            using (MemoryStream ms2 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            {
+                var clipboardData1 = new ImageClipboardData(ms1);
+                object clipboardData2 = new ImageClipboardData(ms2);
+
+                bool result = clipboardData1.Equals(clipboardData2);
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public void OverriddenEquals_CheckingEqualityhWhenObjectsAreNotEqual_ReturnsFalse()
+        {
+            using (MemoryStream ms1 = new MemoryStream(new byte[] { 1, 1, 1 }))
+            using (MemoryStream ms2 = new MemoryStream(new byte[] { 3, 1, 1 }))
+            {
+                var clipboardData1 = new ImageClipboardData(ms1);
+                object clipboardData2 = new ImageClipboardData(ms2);
+
+                bool result = clipboardData1.Equals(clipboardData2);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+    }
+}
