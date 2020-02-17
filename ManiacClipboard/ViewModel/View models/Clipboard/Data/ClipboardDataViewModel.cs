0b9604abd1eda0c9bd6653b098d0d6c2a9a8f04a
Pawleshhh <mateusz46668@gmail.com>
2020-02-17 15:35:42 +0100

    Added ClipboardDataViewModel and ClipboardDataViewModel<T> classes.

---
+++ ManiacClipboard/ViewModel/View models/Clipboard/Data/ClipboardDataViewModel.cs
@@ -0,0 +1,94 @@
+﻿
+using ManiacClipboard.Model;
+using System;
+
+namespace ManiacClipboard.ViewModel
+{
+    public abstract class ClipboardDataViewModel : NotifyPropertyChanges
+    {
+
+        #region Constructors
+
+        public ClipboardDataViewModel(ClipboardData clipboardData)
+        {
+            _clipboardData = clipboardData;
+        }
+
+        #endregion
+
+        #region Private fields
+
+        private readonly ClipboardData _clipboardData;
+
+        #endregion
+
+        #region Properties
+
+        public object Data => _clipboardData.Data;
+
+        public ClipboardDataType DataType => _clipboardData.DataType;
+
+        public DateTime CopyTime => _clipboardData.CopyTime;
+
+        public ClipboardSource Source => _clipboardData.Source;
+
+        public string TagName
+        {
+            get => _clipboardData.TagName;
+            set => SetProperty(() => _clipboardData.TagName == value,
+                                    () => _clipboardData.TagName = value);
+        }
+
+        public bool KeepThat
+        {
+            get => _clipboardData.KeepThat;
+            set => SetProperty(() => _clipboardData.KeepThat == value,
+                                    () => _clipboardData.KeepThat = value);
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public override bool Equals(object obj)
+            => _clipboardData.Equals(obj);
+
+        public override int GetHashCode()
+            => _clipboardData.GetHashCode();
+
+        public override string ToString()
+            => _clipboardData.ToString();
+
+        protected override void DisposeUnmanaged()
+        {
+            _clipboardData.Dispose();
+        }
+
+        public static ClipboardData GetModel(ClipboardDataViewModel clipboardDataVM)
+            => clipboardDataVM._clipboardData;
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+    }
+
+    public abstract class ClipboardDataViewModel<T> : ClipboardDataViewModel
+    {
+        #region Constructors
+
+        protected ClipboardDataViewModel(ClipboardData<T> clipboardData) : base(clipboardData)
+        {
+            Data = clipboardData.Data;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public new T Data { get; }
+
+        #endregion
+    }
+}
