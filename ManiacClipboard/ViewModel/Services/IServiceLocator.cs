43845de42d2217befd5ebad05ec717c9990c2e4b
Pawleshhh <mateusz46668@gmail.com>
2020-02-20 15:51:24 +0100

    Added the MainViewModel class with the IServiceLocation interface.

---
+++ ManiacClipboard/ViewModel/Services/IServiceLocator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManiacClipboard.ViewModel
+{
+    public interface IServiceLocator
+    {
+
+        IClipboardService GetClipboardService();
+
+    }
+}
---
+++ ManiacClipboard/ViewModel/View models/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ManiacClipboard.ViewModel
+{
+    public class MainViewModel : NotifyPropertyChanges
+    {
+
+        #region Constructors
+
+        public MainViewModel(IServiceLocator serviceLocator)
+        {
+            if (serviceLocator == null)
+                throw new ArgumentNullException("serviceLocator");
+
+            ClipboardManager = new ClipboardManagerViewModel(serviceLocator.GetClipboardService());
+        }
+
+        #endregion
+
+        #region Private fields
+
+        private string _title;
+
+        #endregion
+
+        #region Properties
+
+        public ClipboardManagerViewModel ClipboardManager { get; }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(() => _title == value, () => _title = value);
+        }
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Private methods
+
+        protected override void DisposeUnmanaged()
+        {
+            ClipboardManager.Dispose();
+        }
+
+        #endregion
+
+    }
+}
